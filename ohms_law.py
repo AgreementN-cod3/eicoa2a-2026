9dd8ded712d1f1e32d9f938507d4f005b5ec5d74
Agreement_cod3 <you@example.com>
2026-08-30 20:45:15 +0200

    Add error handling and basic tests for Ohm's Law

--- ohms_law.py
+++ ohms_law.py
@@ -13,3 +13,18 @@ Note:  the function raises a Zero division error if current = 0
 assert calculate_resistance(9, 0.03) == 300
 assert calculate_resistance(24, 2) == 12
 print(calculate_resistance.__doc__)
+
+def calc_power(voltage, resistance):
+    """
+    args: voltage(float): voltage across a component in volts (V)
+          resistance(float): resistance throught a component in (Ω)
+    return: it returns the calculated value of power in Watt (W)
+    
+    Note:  the function raises a Zero division error if current = 0
+    """
+    current = voltage / resistance
+
+    return voltage * current
+
+print(calc_power.__doc__)
+
--- test_ohms.py
+++ test_ohms.py
@@ -1,5 +1,12 @@
 from ohms_law import calculate_resistance
+from ohms_law import calc_power
 
-results = calculate_resistance(24, 2)
+results = calculate_resistance(24, 3)
 print("Resistance = ",results, "Ω")
 print(calculate_resistance.__doc__)
+
+ 
+results_P = calc_power(12, results)
+results_P2 = calc_power(15, results)
+print("Power = ",results_P, "W")
+print("Power = ",results_P2, "W")
