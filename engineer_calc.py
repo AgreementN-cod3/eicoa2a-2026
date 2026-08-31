a9e6ae6017a77d4e55a4734052c7f4ccde1406f8
Agreement_cod3 <you@example.com>
2026-08-31 09:44:15 +0200

    Introduce global default current and scope demonstration

--- engineer_calc.py
+++ engineer_calc.py
@@ -1,5 +1,6 @@
 from ohms_law import calculate_resistance
 from unit_converter import mm_to_inches, inches_to_mm
+from unit_converter import cm_to_inches, inches_to_cm
 
 DEFAULT_CURRENT = 0.5
 print("Global value:", DEFAULT_CURRENT)
@@ -15,11 +16,13 @@ def display_menu():
     The menu includes:
         1. calculated resistance (ohm's laws)
         2. Convert length (mm to inches and inches to mm)
-        3. Exit
+        3. Convert length (cm to inches and inches to cm)
+        4. Exit
     """
     print("\n--- Engineering Calculator Menu---" )
     print("1. calculate resistance (ohm's laws)")
-    print("2. Convert length (mm to inches and inches to mm)")    
+    print("2. Convert length (mm to inches and inches to mm)")  
+    print("3. Convert length (cm to inches and inches to cm)")  
     print("3. Exit")  
 
 def main():
@@ -61,7 +64,24 @@ def main():
 
             else: 
                 print("Invalid conversion option")
+
         elif choice == "3":
+             direction2 = input("Enter conversion (cm_to_in or in_to_cm): ")
+             value2 = float(input("Enter the measurement: "))
+
+             if direction2 == "cm_to_in":
+                 results2 = cm_to_inches(value2)
+                 print("Converted value:",round(results2,3), "inches")
+
+             elif direction2 == "in_to_cm":
+                 results2 = inches_to_cm(value2)
+                 print("Converted value:",round(results2,3), "inches")
+
+             else:  print("Invalid option")
+
+
+        
+        elif choice == "4":
             running = False
             print("Program closed")
 
