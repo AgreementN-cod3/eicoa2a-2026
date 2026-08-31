3736f33329be1dcd9c765f56f8e0e506c421659e
Agreement_cod3 <you@example.com>
2026-08-31 09:43:26 +0200

    Add length conversion utilities with docstrings

--- converter_cli.py
+++ converter_cli.py
@@ -1,4 +1,5 @@
 from unit_converter import mm_to_inches, inches_to_mm
+from unit_converter import cm_to_inches, inches_to_cm
 
 direction = input("Enter conversion (mm_to_in or in_to_mm): ")
 
@@ -18,3 +19,20 @@ else:
 print(mm_to_inches.__doc__)
 print(inches_to_mm.__doc__)
 
+direction2 = input("Enter conversion (cm_to_in or in_to_cm): ")
+
+value2 = float(input("Enter the measurement"))
+
+if direction2 == "cm_to_in":
+    results2 = cm_to_inches(value2)
+    print("Converted value:",results2, "inches")
+
+elif direction2 == "in_to_cm":
+    results2 = inches_to_cm(value2)
+    print("Converted value:",results2, "cm")
+
+else:  
+    print("Invalid option")
+
+print(cm_to_inches.__doc__)
+print(inches_to_cm.__doc__)
--- unit_converter.py
+++ unit_converter.py
@@ -13,3 +13,23 @@ def inches_to_mm(inches):
         Return: returns Length in milliemeters(float)(inches * 25.4)   
     """
      return inches * 25.4
+
+def cm_to_inches(cm):
+
+    """
+         Convert Centimetres to inches. 
+         Args:   Lenght in cm(float):
+         Return: returns Length in inches(float)   
+    """
+     
+    return cm / 2.54
+
+def inches_to_cm(inches):
+
+    """
+         Convert inches to cm. 
+         Args:   Lenght in inches (float):
+         Return: returns Length in centimetres(float)   
+    """
+     
+    return inches * 2.54
