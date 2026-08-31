2fd02ea60dc25d89b5f1bd7ae8e2b38932d969dd
Agreement_cod3 <you@example.com>
2026-08-31 10:21:41 +0200

    Menu Implementation

---
+++ build_menu.py
@@ -0,0 +1,12 @@
+
+def build_menu():
+    options = [
+        "1. Calculate Resistance",
+        "2. Convert length (mm to inches and inches to mm)",
+        "3. Convert length (cm to inches and inches to cm)",
+        "4. Exit"   
+    ]
+    for items in options:
+        print(items)
+
+build_menu()
